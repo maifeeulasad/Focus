ddb1b13a45b50d6448932ef6136469b41fc1527c
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-09-19 11:07:22 +0600

    parsing xml data

--- FocusElectron/src/App.tsx
+++ FocusElectron/src/App.tsx
@@ -1,70 +1,63 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import {Moment} from 'moment';
 import electron from '/electron.png'
 import react from '/react.svg'
 import vite from '/vite.svg'
 import styles from 'styles/app.module.scss'
 
+import { readdir, readFile } from 'fs';
+
 const App: React.FC = () => {
-  const [count, setCount] = useState(0)
+  const [logFiles, setLogFiles] = useState<string[]>([]);
+
+  useEffect(()=>{
+    const dir = "D:\\focus\\";
+    readdir(dir,(_,files)=>{
+      setLogFiles(files.map((file)=>dir+file))
+    });
+  },[]);
+
+  const parseRecordsNode = (records: HTMLCollectionOf<Element>) => {
+    // console.log(records);
+    for(let record of records){
+      // console.log(record)
+      console.log(record.getElementsByTagName('process')[0].textContent);
+      console.log(record.getElementsByTagName('timestamp')[0].textContent);
+      // yyyy-MM-dd HH:mm:ss zzz
+    }
+  }
+
+  const parseXml = (xmlString: string) => {
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(xmlString,"text/xml");
+
+
+    xmlDoc.querySelectorAll("start,record,empty").forEach((node: Element)=>{
+      console.log(node)
+      console.log(node.nextElementSibling)
+    })
+
+    
+    // for(let node in xmlDoc){
+    //   console.log(node)
+    // }
+    // // start, record, empty
+    // const starts = xmlDoc.getElementsByTagName('start');
+    // const records = xmlDoc.getElementsByTagName('record');
+    // const empties = xmlDoc.getElementsByTagName('empty');
+    // return parseRecordsNode(records);
+  }
+
+  useEffect(()=>{
+    console.log(logFiles);
+    readFile(logFiles[0], 'utf8' ,(_,data)=>{
+      parseXml(data);
+    })
+  },[logFiles])
 
   return (
     <div className={styles.app}>
-      <header className={styles.appHeader}>
-        <div className={styles.logos}>
-          <div className={styles.imgBox}>
-            <img
-              src={electron}
-              style={{ height: '24vw' }}
-              className={styles.appLogo}
-              alt="electron"
-            />
-          </div>
-          <div className={styles.imgBox}>
-            <img src={vite} style={{ height: '19vw' }} alt="vite" />
-          </div>
-          <div className={styles.imgBox}>
-            <img
-              src={react}
-              style={{ maxWidth: '100%' }}
-              className={styles.appLogo}
-              alt="logo"
-            />
-          </div>
-        </div>
-        <p>Hello Electron + Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <div>
-          <a
-            className={styles.appLink}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className={styles.appLink}
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-          <div className={styles.staticPublic}>
-            Place static files into the{' '}
-            <code>/public</code> folder
-            <img style={{ width: 77 }} src="./node.png" />
-          </div>
-        </div>
-      </header>
+      ping-pong
     </div>
   )
 }
