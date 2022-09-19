568b3abf3b8a9fdb8da34ebba26748d47e396ae4
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-09-19 11:59:26 +0600

    parsing and adding results

--- FocusElectron/src/App.tsx
+++ FocusElectron/src/App.tsx
@@ -1,65 +1,76 @@
-import React, { useState, useEffect } from 'react'
-import {Moment} from 'moment';
-import electron from '/electron.png'
-import react from '/react.svg'
-import vite from '/vite.svg'
-import styles from 'styles/app.module.scss'
+import React, { useState, useEffect } from "react";
+import { readdir, readFile } from "fs";
+import moment from "moment";
 
-import { readdir, readFile } from 'fs';
+import styles from "styles/app.module.scss";
+
+interface TypedDict {
+  [key: string]: number;
+}
 
 const App: React.FC = () => {
   const [logFiles, setLogFiles] = useState<string[]>([]);
+  const [processTime, setProcesstime] = useState<TypedDict>({ unknown: 0 });
 
-  useEffect(()=>{
-    const dir = "D:\\focus\\";
-    readdir(dir,(_,files)=>{
-      setLogFiles(files.map((file)=>dir+file))
+  const format = "yyyy-MM-dd HH:mm:ss zzz";
+  const dir = "D:\\focus\\";
+
+  useEffect(() => {
+    readdir(dir, (_, files) => {
+      setLogFiles(files.map((file) => dir + file));
     });
-  },[]);
+  }, []);
 
-  const parseRecordsNode = (records: HTMLCollectionOf<Element>) => {
-    // console.log(records);
-    for(let record of records){
-      // console.log(record)
-      console.log(record.getElementsByTagName('process')[0].textContent);
-      console.log(record.getElementsByTagName('timestamp')[0].textContent);
-      // yyyy-MM-dd HH:mm:ss zzz
-    }
-  }
+  const measureTime = (nodes: NodeListOf<Element>) => {
+    nodes.forEach((node: Element) => {
+      const nextNode = node.nextElementSibling;
+      if (node.tagName === "record" || node.tagName === "empty") {
+        if (
+          nextNode &&
+          (nextNode.tagName === "record" || nextNode.tagName === "empty")
+        ) {
+          const processName = node.getElementsByTagName("process")[0]
+            .textContent as string;
+          const timeNodeText =
+            node.getElementsByTagName("timestamp")[0].textContent;
+          const timeNextNodeText =
+            nextNode.getElementsByTagName("timestamp")[0].textContent;
+          const timeNode = moment(timeNodeText, format);
+          const timeNodeNext = moment(timeNextNodeText, format);
+          const diff = timeNodeNext.diff(timeNode, "ms");
+          console.log(processName, ' --- ', diff);
+          if (processName in processTime) {
+            setProcesstime((p) => ({
+              ...p,
+              [processName]: diff + processTime[processName],
+            }));
+          } else {
+            setProcesstime((p) => ({ ...p, [processName]: diff }));
+          }
+        }
+      }
+    });
+  };
 
   const parseXml = (xmlString: string) => {
     const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(xmlString,"text/xml");
+    const xmlDoc = parser.parseFromString(xmlString, "text/xml");
 
+    measureTime(xmlDoc.querySelectorAll("start,record,empty"));
+  };
 
-    xmlDoc.querySelectorAll("start,record,empty").forEach((node: Element)=>{
-      console.log(node)
-      console.log(node.nextElementSibling)
-    })
-
-    
-    // for(let node in xmlDoc){
-    //   console.log(node)
-    // }
-    // // start, record, empty
-    // const starts = xmlDoc.getElementsByTagName('start');
-    // const records = xmlDoc.getElementsByTagName('record');
-    // const empties = xmlDoc.getElementsByTagName('empty');
-    // return parseRecordsNode(records);
-  }
-
-  useEffect(()=>{
-    console.log(logFiles);
-    readFile(logFiles[0], 'utf8' ,(_,data)=>{
+  useEffect(() => {
+    readFile(logFiles[0], "utf8", (_, data) => {
       parseXml(data);
-    })
-  },[logFiles])
+    });
+  }, [logFiles]);
 
   return (
     <div className={styles.app}>
       ping-pong
+      <div>{JSON.stringify(processTime)}</div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
