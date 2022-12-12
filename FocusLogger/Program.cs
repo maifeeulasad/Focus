0cb0b194dfa20d38e987aad512a43bfc6b11f4c4
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-12-12 12:39:01 +0600

    change type to windows application, now it runs without console

--- FocusLogger/Program.cs
+++ FocusLogger/Program.cs
@@ -5,18 +5,8 @@ namespace FocusLogger
 {
     class Program
     {
-        [DllImport("kernel32.dll")]
-        static extern IntPtr GetConsoleWindow();
-
-        [DllImport("user32.dll")]
-        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-
-
         static void Main(string[] args)
         {
-            IntPtr handle = GetConsoleWindow();
-            ShowWindow(handle, 0);
-
             new FocusDetection();
         }
     }
